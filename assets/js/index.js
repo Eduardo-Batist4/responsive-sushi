a22a5532521014a3af894d9edc3cb927d4561c9f
Carlos Eduardo Batista <carloswe3st@gmail.com>
2023-07-03 16:06:08 -0300

    Site Finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,8 +16,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
+const sr =  ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 200,
+    reset: true,
+})
 
-
+sr.reveal('.imgHome')
+sr.reveal('.title-home', {origin: 'bottom'})
+sr.reveal('.about-img', {origin: 'left'})
+sr.reveal('.text-about', {origin: 'right'})
+sr.reveal('.container-slider', {origin: 'right'})
+sr.reveal('.email-container', {origin: 'left'})
+sr.reveal('.footer',)
 
 
 
